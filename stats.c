750bb542e90b5d8665ccefca416d80a606237433
Shivalingappa-annaraya <91052331+Shivalingappa-annaraya@users.noreply.github.com>
2021-10-25 10:48:10 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,5 +1,6 @@
 #include "stats.h"
 #include "math.h"
+#inlcude "alerters.h"
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
@@ -23,6 +24,16 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
      }
      s.average = sum/(float)setlength;
     return s;
+}
+
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+	if(computedStats.max > maxThreshold)
+	{
+		alerters[0]();
+		alerters[1]();
+	}
 }
 
 
