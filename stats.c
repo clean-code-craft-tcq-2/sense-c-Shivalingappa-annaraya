d0579f634474a9501e13138e9c381e0d15afaf18
Shivalingappa-annaraya <91052331+Shivalingappa-annaraya@users.noreply.github.com>
2021-10-25 11:02:48 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,10 +1,11 @@
 #include "stats.h"
 #include "math.h"
-#inlcude "alerters.h"
+#include "alerters.h"
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
 
+/*function to calulate min,max,avg*/
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     int i;
@@ -13,7 +14,7 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.min = 0.0;
     s.max = 0.0;
     
-     for(i=0;i<setlenght;i++)
+     for(i=0;i<setlength;i++)
      {
        if(numberset[i]<s.min)
            s.min =numberset[i];
@@ -26,7 +27,7 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     return s;
 }
 
-
+/*function to alert if value is above threshold*/
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
 	if(computedStats.max > maxThreshold)
